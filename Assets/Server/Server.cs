aa4ce1485190d05c060281ddbf0d9c997ae7ac77
novium <mail@novium.pw>
2020-11-15 16:51:22 +0100

    Initial commit

---
+++ Assets/Server/Server.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Server;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting server...");
+        UDPServer server = new UDPServer();
+        server.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Server/UDPServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Assets.Server
+{
+    class UDPServer
+    {
+        IPEndPoint serverEndpoint;
+        UdpClient socket;
+
+        public UDPServer()
+        {
+            this.serverEndpoint = new IPEndPoint(IPAddress.Any, 9000);
+        }
+
+        public void Init()
+        {
+            this.socket = new UdpClient(serverEndpoint);
+            Debug.Log("Started socket on port " + 9000);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024];
+                    data = this.socket.Receive(ref this.serverEndpoint);
+                    Debug.Log(Encoding.ASCII.GetString(data, 0, data.Length));
+                }
+            });
+        }
+    }
+}
