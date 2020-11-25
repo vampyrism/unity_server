1b3e22a6c5aff4fe24b4293affa25c3612e31f36
novium <mail@novium.pw>
2020-11-25 21:09:14 +0100

    Added Client class

---
+++ Assets/Server/Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.Build.Player;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Server
+{
+    public class Client
+    {
+        public IPEndPoint Endpoint { get; private set; }
+        public Queue<Message> MessageQueue { get; private set; }
+        public List<UDPPacket> PacketQueue { get; private set; }
+        public UInt16 RemoteSeqNum { get; private set; }
+        public UInt16 LocalSeqNum { get; private set; }
+
+        public Player player { get; private set; }
+
+        public Client(IPEndPoint endpoint)
+        {
+            this.Endpoint = endpoint;
+            this.MessageQueue = new Queue<Message>();
+            this.PacketQueue = new List<UDPPacket>();
+            this.RemoteSeqNum = 0;
+            this.LocalSeqNum = 0;
+
+            this.player = (Player) PrefabUtility.InstantiatePrefab(Resources.Load("prefabs/Player.prefab"));
+            this.player.Move(1f, 1f);
+        }
+
+        public UDPPacket NextPacket()
+        {
+            return new UDPPacket();
+        }
+
+        /// <summary>
+        /// Adds <c>UDPPacket</c> using Messages in the <c>Client</c>
+        /// message queue.
+        /// </summary>
+        /// <param name="p">packet to append new messages to</param>
+        /// <returns><c>UDPPacket</c> with messages</returns>
+        private UDPPacket BuildPacket(UDPPacket p)
+        {
+            int space = p.SizeLeft();
+
+            while(this.MessageQueue.Count > 0 && this.MessageQueue.Peek().Size() < space)
+            {
+                Message m = this.MessageQueue.Dequeue();
+                p.AddMessage(m);
+            }
+
+            return p;
+        }
+
+        public void SendMessage(Message m)
+        {
+            this.MessageQueue.Enqueue(m);
+        }
+
+        public void AckPacket(UInt16 remoteSeqNum)
+        {
+
+        }
+    }
+}
