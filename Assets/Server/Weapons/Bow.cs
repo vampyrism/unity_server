218cbf6cf2e18b48b6b3d093e123be82f4de9905
root <root@Daniel-PC.localdomain>
2020-12-05 00:16:09 +0100

    Bugfixing and added "attack owner" attribute

---
+++ Assets/Server/Weapons/Bow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asserts.Server
+{
+    public class Bow : Weapon
+    {
+
+        [SerializeField] private Transform projectile;
+        public Bow() {
+            this.weaponName = "bow";
+            this.weaponIndex = 1;
+            this.weaponDamage = 11f;
+            this.reloadSpeed = 0.3f;
+            this.isRanged = true;
+        }
+
+        public override void MakeAttack(Vector2 clickPosition, Vector2 spawnPosition, uint playerId) {
+            Vector2 attackDirection = (clickPosition - (Vector2) spawnPosition).normalized;
+            Transform projectileTransform = Instantiate(projectile, spawnPosition, Quaternion.identity);
+            projectileTransform.GetComponent<Assets.Server.Projectile>().Setup(attackDirection, weaponDamage, playerId);
+        }
+
+    }
+}
